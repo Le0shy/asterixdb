3614975930eed51b3e8982b69843c0cfef421adc
Le0shy <Le0SHY@outlook.com>
2024-10-01 01:07:31 -0700

    add CompacityControllerGuard

--- hyracks-fullstack/hyracks/hyracks-control/hyracks-control-cc/src/main/java/org/apache/hyracks/control/cc/scheduler/CapacityControllerGuard.java
+++ hyracks-fullstack/hyracks/hyracks-control/hyracks-control-cc/src/main/java/org/apache/hyracks/control/cc/scheduler/CapacityControllerGuard.java
@@ -1,6 +1,5 @@
 package org.apache.hyracks.control.cc.scheduler;
 
-import org.apache.hyracks.api.exceptions.ErrorCode;
 import org.apache.hyracks.api.exceptions.HyracksException;
 import org.apache.hyracks.api.job.JobSpecification;
 import org.apache.hyracks.api.job.resource.IClusterCapacity;
@@ -16,7 +15,6 @@ public class CapacityControllerGuard {
     private int CPUQuotaShort = 8;
     private int CPUQuotaCommon;
     private final double memoryAllocatedToShort = 0.1;
-
     private final int maximumAggregatedCores;
 
     private final long maximumAggregatedMemoryByteSize;
@@ -38,109 +36,100 @@ public class CapacityControllerGuard {
         CPUQuotaCommon = maximumAggregatedCores - CPUQuotaShort;
     }
 
-    private void checkIfExceedMaximumCapacity(IReadOnlyClusterCapacity requiredCapacity) throws HyracksException {
-        long reqAggregatedMemoryByteSize = requiredCapacity.getAggregatedMemoryByteSize();
-        int reqAggregatedNumCores = requiredCapacity.getAggregatedCores();
-
-        if (!(reqAggregatedMemoryByteSize <= maximumAggregatedMemoryByteSize
-                && reqAggregatedNumCores <= maximumCapacity.getAggregatedCores())) {
-            throw HyracksException.create(ErrorCode.JOB_REQUIREMENTS_EXCEED_CAPACITY, requiredCapacity.toString(),
-                    maximumCapacity.toString());
-        }
-    }
     public IJobCapacityController.JobSubmissionStatus allocate(JobRun jobRun) throws HyracksException {
         JobSpecification job = jobRun.getJobSpecification();
-        IReadOnlyClusterCapacity requiredCapacity = job.getRequiredClusterCapacity();
-        /* check if the required capacity exceeds maximum */
-        checkIfExceedMaximumCapacity(requiredCapacity);
 
         /* Short jobs */
         if(jobRun.getSchedulingType() == JobTypeManager.JobSchedulingType.SHORT) {
-            /* check cpu quota and memory available */
-            if (CPUQuotaShort < requiredCapacity.getAggregatedCores() ||
-                    memoryAvailableShort < requiredCapacity.getAggregatedMemoryByteSize()) {
-                return IJobCapacityController.JobSubmissionStatus.QUEUE;
-            }
-            /* then check resource manager */
-            if (checkResourceManager(jobRun.getJobSpecification().getRequiredClusterCapacity())
-                    == IJobCapacityController.JobSubmissionStatus.EXECUTE) {
-                /* record resource usage for short jobs */
-                allocateResourceForShortJob(requiredCapacity.getAggregatedMemoryByteSize(),
-                        requiredCapacity.getAggregatedCores());
-                return IJobCapacityController.JobSubmissionStatus.EXECUTE;
-            } else {
-                return IJobCapacityController.JobSubmissionStatus.QUEUE;
-            }
+            return allocateResourceForShortJob(job);
         }
+
         /* Other jobs */
         else {
-            if (CPUQuotaCommon < requiredCapacity.getAggregatedCores() ||
-                    memoryAvailableCommon < requiredCapacity.getAggregatedMemoryByteSize()) {
-                return IJobCapacityController.JobSubmissionStatus.QUEUE;
-            }
-            if (checkResourceManager(jobRun.getJobSpecification().getRequiredClusterCapacity())
-                    == IJobCapacityController.JobSubmissionStatus.EXECUTE) {
-                /* record resource usage for common jobs */
-                allocateResourceForCommonJob(requiredCapacity.getAggregatedMemoryByteSize(),
-                        requiredCapacity.getAggregatedCores());
-                return IJobCapacityController.JobSubmissionStatus.EXECUTE;
-            } else {
-                return IJobCapacityController.JobSubmissionStatus.QUEUE;
-            }
+            return allocateResourceForCommonJob(job);
         }
     }
 
-    public IJobCapacityController.JobSubmissionStatus checkResourceManager(IClusterCapacity requiredCapacity) throws HyracksException {
-        long reqAggregatedMemoryByteSize = requiredCapacity.getAggregatedMemoryByteSize();
-        int reqAggregatedNumCores = requiredCapacity.getAggregatedCores();
-
-        IClusterCapacity currentCapacity = resourceManager.getCurrentCapacity();
-        long currentAggregatedMemoryByteSize = currentCapacity.getAggregatedMemoryByteSize();
-        int currentAggregatedAvailableCores = currentCapacity.getAggregatedCores();
-
-        if (!(reqAggregatedMemoryByteSize <= currentAggregatedMemoryByteSize
-                && reqAggregatedNumCores <= currentAggregatedAvailableCores)) {
-            return IJobCapacityController.JobSubmissionStatus.QUEUE;
-        }
-        currentCapacity.setAggregatedMemoryByteSize(currentAggregatedMemoryByteSize - reqAggregatedMemoryByteSize);
-        currentCapacity.setAggregatedCores(currentAggregatedAvailableCores - reqAggregatedNumCores);
-        return IJobCapacityController.JobSubmissionStatus.EXECUTE;
-    }
-
-    private void allocateResourceForShortJob(long reqAggregatedMemoryByteSize, int reqAggregatedNumCores) {
-        memoryAvailableShort -= reqAggregatedMemoryByteSize;
-        CPUQuotaShort -= reqAggregatedNumCores;
-    }
-
-    private void allocateResourceForCommonJob(long reqAggregatedMemoryByteSize, int reqAggregatedNumCores) {
-        memoryAvailableCommon -= reqAggregatedMemoryByteSize;
-        CPUQuotaCommon -= reqAggregatedNumCores;
-    }
-
-    public void release(JobSpecification job) {
+    private IJobCapacityController.JobSubmissionStatus allocateResourceForShortJob(JobSpecification job)
+            throws HyracksException {
         IClusterCapacity requiredCapacity = job.getRequiredClusterCapacity();
         long reqAggregatedMemoryByteSize = requiredCapacity.getAggregatedMemoryByteSize();
         int reqAggregatedNumCores = requiredCapacity.getAggregatedCores();
-        IClusterCapacity currentCapacity = resourceManager.getCurrentCapacity();
-        long aggregatedMemoryByteSize = currentCapacity.getAggregatedMemoryByteSize();
-        int aggregatedNumCores = currentCapacity.getAggregatedCores();
-        currentCapacity.setAggregatedMemoryByteSize(aggregatedMemoryByteSize + reqAggregatedMemoryByteSize);
-        currentCapacity.setAggregatedCores(aggregatedNumCores + reqAggregatedNumCores);
-        ensureMaxCapacity();
+
+        /* check cpu quota and memory available */
+        if (CPUQuotaShort < requiredCapacity.getAggregatedCores() ||
+                memoryAvailableShort < requiredCapacity.getAggregatedMemoryByteSize()) {
+            return IJobCapacityController.JobSubmissionStatus.QUEUE;
+        }
+
+        /* call capacityController's allocate */
+        if (checkCapacityController(job)
+                == IJobCapacityController.JobSubmissionStatus.EXECUTE) {
+            /* record resource usage for short jobs */
+            memoryAvailableShort -= reqAggregatedMemoryByteSize;
+            CPUQuotaShort -= reqAggregatedNumCores;
+            return IJobCapacityController.JobSubmissionStatus.EXECUTE;
+        } else {
+            return IJobCapacityController.JobSubmissionStatus.QUEUE;
+        }
     }
 
-    private void ensureMaxCapacity() {
-        final IClusterCapacity currentCapacity = resourceManager.getCurrentCapacity();
-        final IReadOnlyClusterCapacity maximumCapacity = resourceManager.getMaximumCapacity();
-        if (currentCapacity.getAggregatedCores() > maximumCapacity.getAggregatedCores()
-                || currentCapacity.getAggregatedMemoryByteSize() > maximumCapacity.getAggregatedMemoryByteSize()) {
-            LOGGER.warn("Current cluster available capacity {} is more than its maximum capacity {}", currentCapacity,
-                    maximumCapacity);
+    private IJobCapacityController.JobSubmissionStatus allocateResourceForCommonJob(JobSpecification job)
+            throws HyracksException {
+        IClusterCapacity requiredCapacity = job.getRequiredClusterCapacity();
+        long reqAggregatedMemoryByteSize = requiredCapacity.getAggregatedMemoryByteSize();
+        int reqAggregatedNumCores = requiredCapacity.getAggregatedCores();
+
+        /* check cpu quota and memory available */
+        if (CPUQuotaCommon < requiredCapacity.getAggregatedCores() ||
+                memoryAvailableCommon < requiredCapacity.getAggregatedMemoryByteSize()) {
+            return IJobCapacityController.JobSubmissionStatus.QUEUE;
         }
+
+        /* then check resource manager */
+        if (checkCapacityController(job)
+                == IJobCapacityController.JobSubmissionStatus.EXECUTE) {
+            /* record resource usage for common jobs */
+            memoryAvailableCommon -= reqAggregatedMemoryByteSize;
+            CPUQuotaCommon -= reqAggregatedNumCores;
+            return IJobCapacityController.JobSubmissionStatus.EXECUTE;
+        } else {
+            return IJobCapacityController.JobSubmissionStatus.QUEUE;
+        }
+    }
+
+    private IJobCapacityController.JobSubmissionStatus checkCapacityController(JobSpecification job)
+            throws HyracksException {
+        return jobCapacityController.allocate(job);
+    }
+
+    public void release(JobRun jobRun) {
+        IClusterCapacity requiredCapacity = jobRun.getJobSpecification().getRequiredClusterCapacity();
+        long reqAggregatedMemoryByteSize = requiredCapacity.getAggregatedMemoryByteSize();
+        int reqAggregatedNumCores = requiredCapacity.getAggregatedCores();
+
+        /* Short jobs */
+        if(jobRun.getSchedulingType() == JobTypeManager.JobSchedulingType.SHORT) {
+            memoryAvailableShort += reqAggregatedMemoryByteSize;
+            CPUQuotaShort += reqAggregatedNumCores;
+            jobCapacityController.release(jobRun.getJobSpecification());
+        }
+
+        /* Other jobs */
+        else {
+            memoryAvailableCommon += reqAggregatedMemoryByteSize;
+            CPUQuotaCommon += reqAggregatedNumCores;
+            jobCapacityController.release(jobRun.getJobSpecification());
+        }
+
     }
 
     public double getMemoryRatio(JobSpecification jobSpecification) {
         return (double) jobSpecification.getRequiredClusterCapacity().getAggregatedMemoryByteSize()
                 / maximumAggregatedMemoryByteSize;
+    }
+
+    public boolean isSQAResourcesAvailable() {
+        return CPUQuotaShort >= 4;
     }
 }
